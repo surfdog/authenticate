db6a900a03dd584ace4f88807d7dfd9f9542d164
Marc Tibbitts <marc@shiphawk.com>
2016-11-08 13:38:57 -0800

    still trying

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server 'ip-172-31-0-170', user: 'authenticate', roles: %w{app db}
+server '54.153.108.164', user: 'authenticate', roles: %w{app db}
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
