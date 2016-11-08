910c3be7cc98460d2694e5fc2fa57ef4014b2b2f
Marc Tibbitts <marc@shiphawk.com>
2016-11-08 13:17:31 -0800

    change approach

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-# server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
+server '54.153.108.164', user: 'authenticate', roles: %w{app db}
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
@@ -17,9 +17,9 @@
 # property set. Specify the username and a domain or IP for the server.
 # Don't use `:all`, it's a meta role.
 
-role :app, %w{authenticate@ec2-54-153-108-164.us-west-1.compute.amazonaws.com}, my_property: :my_value
-# role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
-role :db,  %w{authenticate@ec2-54-153-108-164.us-west-1.compute.amazonaws.com}
+#role :app, %w{authenticate@ec2-54-153-108-164.us-west-1.compute.amazonaws.com}, my_property: :my_value
+#role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
+#role :db,  %w{authenticate@ec2-54-153-108-164.us-west-1.compute.amazonaws.com}
 
 
 
